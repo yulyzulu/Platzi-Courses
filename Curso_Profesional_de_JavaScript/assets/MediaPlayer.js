a40954b34691acfe6c72eef80b17f85aa4147fba
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-11-08 22:14:22 -0500

    JavaScript Course

---
+++ Curso_Profesional_de_JavaScript/assets/MediaPlayer.js
@@ -0,0 +1,14 @@
+
+function MediaPlayer(config) {
+  this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function() {
+  if (this.media.paused) {
+    this.media.play();
+  } else {
+    this.media.pause()
+  }
+}
+
+export default MediaPlayer;
