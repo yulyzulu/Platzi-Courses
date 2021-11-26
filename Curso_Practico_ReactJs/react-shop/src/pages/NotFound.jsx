f44ddd73fa05a07c3209d3de13304e38dcc5be6a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-11-25 21:55:48 -0500

    React Course

---
+++ Curso_Practico_ReactJs/react-shop/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <section>
+      <h1>Not Found</h1>
+      <p>Error 404</p>
+    </section>
+  );
+};
+
+export default NotFound;
