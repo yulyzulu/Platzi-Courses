8812599e669535210f8fc0d7be43c2f7de8180b6
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-12-01 21:57:20 -0500

    React

--- Curso_Practico_ReactJs/react-shop/src/containers/MyOrder.jsx
+++ Curso_Practico_ReactJs/react-shop/src/containers/MyOrder.jsx
@@ -8,7 +8,13 @@ import arrow from '@icons/flechita.svg';
 const MyOrder = () => {
 
 	const {state} = useContext(AppContext);
-	console.log(state)
+	//console.log(state);
+
+	const sumTotal = () => {
+		const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	}
 
 	return (
 		<aside className="MyOrder">
@@ -24,7 +30,7 @@ const MyOrder = () => {
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>$ {sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
